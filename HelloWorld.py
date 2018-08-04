1b55355040fb1df41cbed3a86d2a245a084f8102
lekhaj <lekhaj.nadimpalli@gmail.com>
2018-08-03 23:46:36 -0400

    initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+greeting = 'Hello '
+
+name = input('PLease enter you name ') # input function is used to take the values entered from the keyboard
+
+print(greeting + name)
+
+splitString = 'I am\nUsing Split String\nenjoy the lesson '
+
+tabbedString = '1\t2\t3\t4\t5'
+
+print(splitString + tabbedString)
