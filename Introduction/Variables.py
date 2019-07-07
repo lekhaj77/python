2b191ebcb4d080f758b52a0f2c7bd5e7221b3beb
lekhaj <lekhaj.nadimpalli@gmail.com>
2019-07-07 20:01:45 +0530

    checkin Loops

--- Introduction/Variables.py
+++ Introduction/Variables.py
@@ -1,10 +1,10 @@
-#Integers
+# Integers
 
-a=12
-b=3
+a = 12
+b = 3
 
-print(a+b)
-print(a*b)
-print(a/b)  # this gives 4.0
-print(a//b) # // --> is used to print the whole number eg: 4
-print(a%b)
+print(a + b)
+print(a * b)
+print(a / b)  # this gives 4.0
+print(a // b)  # // --> is used to print the whole number eg: 4
+print(a % b)
