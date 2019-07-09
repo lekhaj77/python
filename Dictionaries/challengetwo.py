5ee215f47dc521037fc17beb0db549a39d14b1d5
lnadimp2 <lekhaj_nadimpalli@premierinc.com>
2019-07-09 12:19:19 +0530

    dictionaries

--- Dictionaries/challengetwo.py
+++ Dictionaries/challengetwo.py
@@ -38,30 +38,30 @@ vocabulary = {"QUIT": "Q",
               "VALLEY": "4",
               "FOREST": "5"}
  
-# loc = 1
-# while True:
-#     availableExits = ", ".join(exits[loc].keys())
-#
-#     print(locations[loc])
-#
-#     if loc == 0:
-#         break
-#     else:
-#         allExits = exits[loc].copy()
-#         allExits.update(namedExits[loc])
-#
-#     direction = input("Available exits are " + availableExits).upper()
-#     print()
-#
-#     # Parse the user input, using our vocabulary dictionary if necessary
-#     if len(direction) > 1:  # more than 1 letter, so check vocab
-#         words = direction.split()
-#         for word in words:
-#             if word in vocabulary:   # does it contain a word we know?
-#                 direction = vocabulary[word]
-#                 break
-#
-#     if direction in allExits:
-#         loc = allExits[direction]
-#     else:
-#         print("You cannot go in that direction")
+loc = 1
+while True:
+    availableExits = ", ".join(locations[loc]['exits'].keys())
+
+    print(locations[loc]['desc'])
+
+    if loc == 0:
+        break
+    else:
+        allExits = locations[loc]['exits'].copy()
+        allExits.update(locations[loc]['namedExits'])
+
+    direction = input("Available exits are " + availableExits).upper()
+    print()
+
+    # Parse the user input, using our vocabulary dictionary if necessary
+    if len(direction) > 1:  # more than 1 letter, so check vocab
+        words = direction.split()
+        for word in words:
+            if word in vocabulary:   # does it contain a word we know?
+                direction = vocabulary[word]
+                break
+
+    if direction in allExits:
+        loc = allExits[direction]
+    else:
+        print("You cannot go in that direction")
